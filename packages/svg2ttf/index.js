4b8413e227b26971c23c84bbe6a4c4566c433fcc
wsx <wengshixiong@163.com>
2024-05-07 19:19:31 +0800

    feat: 添加svg2font工具

---
+++ packages/svg2ttf/index.js
@@ -0,0 +1,22 @@
+const svgtofont = require("svgtofont");
+const path = require("path");
+
+const _unicodeMap = {
+  您: '\u60a8',
+};
+
+svgtofont({
+  src: path.resolve(__dirname, "svg/from"), // svg path
+  dist: path.resolve(__dirname, "svg/to"), // output path
+  styleTemplates: path.resolve(__dirname, "styles"), // file templates path (optional)
+  fontName: "htnova-yuan", // font name
+  css: true, // Create CSS files.
+  useNameAsUnicode: true,
+  svgicons2svgfont: {
+    fontHeight: 1000,
+    // ascent: 1000,
+    // normalize: true,
+    centerVertically: true,
+    centerVertically: true
+  },
+});
